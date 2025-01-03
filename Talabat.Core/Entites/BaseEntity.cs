f7c1faf0e8c1ab52fec717c951987913be848836
Mohamed Ali <mohamedalibk66@gmail.com>
2025-01-03 08:55:41 +0200

    Product Module

---
+++ Talabat.Core/Entites/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entites
+{
+    public class BaseEntity
+    {
+      public int id { get; set; }
+
+    }
+}
