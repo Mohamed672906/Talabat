42a5aebc0026f1e528f0083cbc9f62b97eefe205
Mohamed Ali <mohamedalibk66@gmail.com>
2025-01-03 07:37:56 +0200

    Add project files.

---
+++ Talabat.ABIS/Program.cs
@@ -0,0 +1,45 @@
+namespace Talabat.ABIS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            #region Configer Services
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(); 
+            #endregion
+
+
+
+            var app = builder.Build();
+
+            #region Configure
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers(); 
+            #endregion
+
+
+            app.Run();
+        }
+    }
+}
