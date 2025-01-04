9ee744eef08f995701fad244162d68e92a824be9
Mohamed Ali <mohamedalibk66@gmail.com>
2025-01-04 06:45:47 +0200

    Generic Repository

---
+++ Talabat.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entites;
+
+namespace Talabat.Core.Repositories
+{
+    public interface IGenericRepository<T> where T :BaseEntity
+    {
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(int Id);
+
+
+
+
+    }
+}
---
+++ Talabat.Repository/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Talabat.Core.Entites;
+using Talabat.Core.Repositories;
+using Talabat.Repository.Data;
+
+namespace Talabat.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _dbcontext;
+
+        public GenericRepository(StoreContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+
+
+
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+
+            return await _dbcontext.Set<T>().ToListAsync(); 
+
+        }
+
+        public async Task<T> GetByIdAsync(int Id)
+        {
+            return await _dbcontext.Set<T>().FindAsync(Id);
+        }
+    }
+}
