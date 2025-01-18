35560f1c56ec18342918a10ebc3db1edae3a39cc
Mohamed Ali <mohamedalibk66@gmail.com>
2025-01-18 05:48:07 +0200

    Order Module Entities Configurartion

---
+++ Talabat.Repository/Data/Configrations/DeliveryMethodConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Talabat.Core.Entites.Order;
+
+namespace Talabat.Repository.Data.Configrations
+{
+    public class DeliveryMethodConfigurations : IEntityTypeConfiguration<DeliveryMethod>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+        {
+            builder.Property(Dm => Dm.Cost).HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ Talabat.Repository/Data/Configrations/OrderItemConfigurations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Talabat.Core.Entites.Order;
+
+namespace Talabat.Repository.Data.Configrations
+{
+    public class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.OwnsOne(OI => OI.product, P => P.WithOwner());
+            builder.Property(OI => OI.price).HasColumnType("decimal(18,2)");
+        }
+
+
+
+    }
+}
