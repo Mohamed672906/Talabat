69fe62603284fb1fc5ffa0e6635847cec1158680
Mohamed Ali <mohamedalibk66@gmail.com>
2025-01-21 00:17:13 +0200

    Get Orders For Specific User Function

---
+++ Talabat.Core/Specifications/OrderSpecifications/OrderSpecification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entites.Order;
+
+namespace Talabat.Core.Specifications.OrderSpecifications
+{
+    public class OrderSpecification : BaseSepecification<Order>
+    {
+
+        public OrderSpecification(string email) : base(O => O.BuyerEmail == email)
+        {
+            Includes.Add(O => O.Items);
+            Includes.Add(O => O.DeliveryMethod);
+            AddOrderByDescending(O => O.OrderDate);
+
+        }
+        public OrderSpecification(string email, int orderId) : base(O => O.BuyerEmail == email && O.Id == orderId)
+        {
+            Includes.Add(O => O.Items);
+            Includes.Add(O => O.DeliveryMethod);
+
+        }
+
+
+
+
+
+    }
+}
