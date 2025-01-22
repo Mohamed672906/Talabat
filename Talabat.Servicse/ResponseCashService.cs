e7c995097f79e5def5b49f56a46a28a7c4791a9c
Mohamed Ali <mohamedalibk66@gmail.com>
2025-01-22 05:37:05 +0200

    Part 02 Caching Service

---
+++ Talabat.Servicse/ResponseCashService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+using Talabat.Core.Service;
+
+namespace Talabat.Servicse
+{
+    public class ResponseCashService : IResponseCashService
+    {
+        private readonly IDatabase _database;
+
+        public ResponseCashService(IConnectionMultiplexer Redis)
+        {
+            _database = Redis.GetDatabase();
+
+        }
+
+
+        public async Task CasheResponseAsync(string Cashkey, object Responce, TimeSpan ExpireTime)
+        {
+            if (Responce is null) return;
+            var Options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var SerilzedResopnse = JsonSerializer.Serialize(Responce, Options);
+
+            await _database.StringSetAsync(Cashkey, SerilzedResopnse, ExpireTime);
+
+
+        }
+
+        public async Task<string> GetCashResopnse(string Cashkey)
+        {
+            var CasheResopnse = await _database.StringGetAsync(Cashkey);
+            if (CasheResopnse.IsNullOrEmpty) return null;
+            return CasheResopnse;
+        }
+    }
+}
